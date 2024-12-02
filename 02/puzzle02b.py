8b2304dd4396950f5f0ab3a835d26d96d94509c0
Christian Macht <christian.macht@gmail.com>
2024-12-02 20:39:43 +0100

    day 2

--- 02/puzzle02b.py
+++ 02/puzzle02b.py
@@ -1,54 +1,23 @@
 from itertools import pairwise
 
-with open("input-training") as f:
-    nums = [lvl.split() for lvl in f.read().splitlines()]
+with open("input") as f:
+    # deltas = [[int(b) - int(a) for a, b in pairwise(levels.split())] for levels in f.read().splitlines()]
+    levels_list = [[int(i) for i in l.split()] for l in f.read().splitlines()]
 
-print(nums)
-sum = 0
-for lvl in nums:
-    failed = False
-    decreasing = None
-    increasing = None
-    joker = True
-    counter = 0
+print(levels_list)
+ans = 0
 
-    while counter < len(lvl):
-        print(lvl)
-        x = lvl[counter]
-        y = lvl[counter + 1]
-        print(counter, x, y)
-        diff = int(x) - int(y)
-        if diff < 0 and decreasing is None:
-            decreasing = True
-        if diff > 0 and increasing is None:
-            increasing = True
-        if increasing is True and decreasing is True:
-            failed = True
-            print('failed inc_dec at', counter)
-        if abs(diff) >= 1 and abs(diff) <= 3:
-            pass
-        else:
-            failed = True
-            print('failed limit at', counter)
+def is_safe(levels):
+    print(levels)
+    reversion_ok = (levels==sorted(levels) or levels==sorted(levels, reverse=True))
+    limit_ok = True
+    deltas = [y - x for x, y in pairwise(levels)]
+    if not all(0 < abs(x) <= 3 for x in deltas):
+        limit_ok = False
+    return reversion_ok and limit_ok
 
-        if failed and joker:
-            joker = False
-            del lvl[counter + 1]
-            print('try again')
-            counter = 0
-            increasing = None
-            decreasing = None
-            failed = False
-            continue
-        if counter < len(lvl) - 2:
-            counter += 1
-            continue
-        else:
-            break
+for levels in levels_list:
+    if any(is_safe(levels[:idx] + levels[idx+1:]) for idx in range(len(levels))):
+        ans += 1
 
-    if not failed:
-        sum += 1
-        print('--- added! ---')
-    else:
-        print('--- not added! ---')
-print(sum)
+print('ans:', ans)
