6116fbfa85808d47448a3f5bcfae30961d080a4a
Christian Macht <christian.macht@gmail.com>
2024-12-09 20:41:55 +0100

    day9 unfinished

--- 09/puzzle09b.py
+++ 09/puzzle09b.py
@@ -1,35 +1,60 @@
 with open("input") as f:
     line = f.readline().strip()
 
-print(line)
-d_id = 0
+fid = 0
 disk = []
-files = 0
+blocks = 0
 for i, n in enumerate(line):
     if i % 2 == 0:
-        disk.extend([d_id] * int(n))
-        d_id += 1
-        files += 1 * int(n)
+        if fid == 0:
+            starter = [fid] * int(n)
+            fid += 1
+        else:
+            disk.extend([fid] * int(n))
+            fid += 1
+            blocks += 1 * int(n)
     else:
-        disk.extend([''] * int(n))
+        disk.extend([0] * int(n))
 
 print(disk)
+print(starter)
+fid = max(disk)
+print(f'{fid=}')
 print(len(disk))
-print(files)
+print(blocks)
+
+
+def find_gap_left(flen, limit):
+    glen = 0
+    for k in range(limit):
+        if disk[k] == 0:
+            glen += 1
+        else:
+            glen = 0
+        if glen >= flen:
+            return k - glen + 1
+    return None
 
 l = 0
+flen = 0
 for r in reversed(range(len(disk))):
-    v = disk[r]
-    if not isinstance(v, int): continue
-    disk[r] = ''
-    for left in range(l, files):
-        if not isinstance(disk[left], int):
-            l = left
-            break
-    disk[l] = v
+    if flen > 0 and disk[r] != fid:
+        idx = find_gap_left(flen, r)
+        if idx is not None:
+            for m in range(flen):
+                disk[idx + m] = fid
+                disk[r + 1 + m] = 0
+        fid -= 1
+        flen = 0
+        # print(f'{flen=}', r)
+        # print(f'{idx=}')
+    if disk[r] == fid:
+        flen += 1
 
+disk = starter + disk
+print(disk)
 ans = 0
-for i in range(files):
-    ans += disk[i] * i
-
+for i, e in enumerate(disk):
+    ans += i * e
 print(ans)
+
